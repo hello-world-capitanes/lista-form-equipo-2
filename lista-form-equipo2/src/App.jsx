1a1f7b93a4bd2d718a752d39b9fffac475cc7dfe
Nicole Struggia <nicole.struggia@babelgroup.com>
2023-10-04 12:19:30 +0200

    adding changes

--- lista-form-equipo2/src/App.jsx
+++ lista-form-equipo2/src/App.jsx
@@ -1,34 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Modal from './components/Modal'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [show, setShow] = useState(false)
+
+  const handleModalShow = (boolean) => {
+    setShow((prevShow) => prevShow = boolean)
+    console.log(show)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='container d-flex'>
+      {show ? <Modal handleModalShow={handleModalShow} /> : <div onDoubleClick={() => handleModalShow(true)} className='test'></div>}
+    </div>
   )
 }
 
---
+++ lista-form-equipo2/src/components/Modal.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Title from './Title'
+
+const Modal = () => {
+  return (
+    <div>
+        <Title title={'Modal'}/>
+    </div>
+  )
+}
+
+export default Modal
